3335b652ec9adf2672b31193328d6b9a11f0b880
meritissimo1 <marcelomoreira1905@gmail.com>
2023-03-15 18:04:12 -0400

    add function identifier; created dir scenes with test.rt

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,11 +6,33 @@
 /*   By: marcrodr <marcrodr@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 14:54:46 by marcrodr          #+#    #+#             */
-/*   Updated: 2023/03/15 16:48:10 by marcrodr         ###   ########.fr       */
+/*   Updated: 2023/03/15 17:57:22 by marcrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
+
+static int parse_file(char *line)
+{
+	char	**tokens;
+
+	tokens = ft_split(line, ' ');
+	if(!tokens)
+		return (0);
+	if (ft_strncmp(tokens[0], "A", 1) == 0)
+		printf("Letter A -> %s\n", line);
+	if (ft_strncmp(tokens[0], "C", 1) == 0)
+		printf("Letter C -> %s\n", line);
+	if (ft_strncmp(tokens[0], "L", 1) == 0)
+		printf("Letter L -> %s\n", line);
+	if (ft_strncmp(tokens[0], "sp", 1) == 0)
+		printf("Letter sp -> %s\n", line);
+	if (ft_strncmp(tokens[0], "pl", 1) == 0)
+		printf("Letter pl -> %s\n", line);
+	if (ft_strncmp(tokens[0], "cy", 1) == 0)
+		printf("Letter cy -> %s\n", line);
+	return (0);
+}
 
 int	read_file(char *filename)
 {
@@ -23,7 +45,8 @@ int	read_file(char *filename)
 		line = gnl(fd);
 		if (!line)
 			return (0);
-		printf("%s", line);
+	//	printf("%s", line);
+		parse_file(line);
 	}
 	return (0);
 }

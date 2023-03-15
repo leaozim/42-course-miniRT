37c9584157d3df2ea7757992fc4726ae8c0cc7fd
Larissa Silva <larissa_silva@outlook.com>
2023-03-15 15:02:34 -0300

    add test files

---
+++ test/test_/test_soma.c
@@ -0,0 +1,21 @@
+# include "../h_test.h"
+
+
+int	soma(int a, int b)
+{
+	return (a + b);
+}
+
+void	valid_soma(void)
+{
+	int a = 3;
+	int b = 3;
+	int result = 6;
+	TEST_ASSERT_EQUAL_INT(result, soma(a, b));
+}
+
+void test_soma(void)
+{
+
+	RUN_TEST(valid_soma);
+}

3c0b47d5d7119705e02b1b68963d87766fd879e2
meritissimo1 <marcelomoreira1905@gmail.com>
2023-03-20 11:18:22 -0400

    set ratio data

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -58,16 +58,13 @@ int	is_invalid_file_data(char	**tokens)
 
 int	check_id_a(char **tokens, t_scene *scene)
 {
-	double	floating;
-
-	floating = 0.0;
 	if (is_invalid_file_data(tokens))
 		return (error_msg("File data is invalid"), ERROR);
 	scene->ambient = (t_ambient *)ft_calloc(1, sizeof(t_ambient));
 	if (!ft_isfloat(tokens[1]))
 		return (error_msg("Parameter needs to be a Float"), ERROR);
-	floating = ft_atof(tokens[1]);
-	if (ft_isrange(floating, 0, 1))
+	scene->ambient->ratio = ft_atof(tokens[1]);
+	if (ft_isrange(scene->ambient->ratio, 0, 1))
 		return (error_msg("Incorrect range of numbers"), ERROR);
 	check_color(tokens[2]);
 	return (0);

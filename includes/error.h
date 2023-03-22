bcc9fd0dd212344b4a73eeb7b37c14b1847d6da8
Larissa Silva <larissa_silva@outlook.com>
2023-03-22 18:04:25 -0300

    add updates

--- includes/error.h
+++ includes/error.h
@@ -5,24 +5,24 @@
 # define ERROR_FLOAT "Parameter for each x,y,z axis needs to be a Float!"
 # define ERROR_RANGE_COLOR "Incorrect range of RGB [0-255]!"
 
-#define ERROR_INVALID_RGB "Invalid RGB!"
-#define ERROR_MANY_ARGC_C "To many or few arguments for camera!"
+# define ERROR_INVALID_RGB "Invalid RGB!"
+# define ERROR_MANY_ARGC_C "To many or few arguments for camera!"
 
-#define ERROR_FILE "File data are invaded!"
-#define ERROR_COOR_C "Camera coordinates are invaded!"
+# define ERROR_FILE "File data are invaded!"
+# define ERROR_COOR_C "Camera coordinates are invaded!"
 
-#define ERROR_FOV "FOV is invalid!"
-#define ERROR_MANY_ARGC_CY "To many or few arguments for cylinder!"
-#define ERROR_COOR_CY "Cylinder coordinates are invaded!"
-#define ERROR_FLOAT_CY "Parameter needs to be a Float!"
+# define ERROR_FOV "FOV is invalid!"
+# define ERROR_MANY_ARGC_CY "To many or few arguments for cylinder!"
+# define ERROR_COOR_CY "Cylinder coordinates are invaded!"
+# define ERROR_FLOAT_CY "Parameter needs to be a Float!"
 
-#define ERROR_MANY_ARGC_SP "To many or few arguments for sphere!"
-#define ERROR_ORI_SP "Sphere orientation are invalid!"
-#define ERROR_FLOAT_SP "Parameter for sphere needs to be a Float"
+# define ERROR_MANY_ARGC_SP "To many or few arguments for sphere!"
+# define ERROR_ORI_SP "Sphere orientation are invalid!"
+# define ERROR_FLOAT_SP "Parameter for sphere needs to be a Float"
 
-#define ERROR_FORMAT "The scene needs to be in *.rt format"
+# define ERROR_FORMAT "The scene needs to be in *.rt format"
 
-#define ERROR_MANY_ARGC "Too many arguments!"
-#define ERROR_NOT_FILE "Need to add scene file!"
+# define ERROR_MANY_ARGC "Too many arguments!"
+# define ERROR_NOT_FILE "Need to add scene file!"
 
 #endif

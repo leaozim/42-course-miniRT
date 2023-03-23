631c11d53e3bc5999f84840e5a59c587d3016de1
meritissimo1 <marcelomoreira1905@gmail.com>
2023-03-23 15:58:46 -0400

    fixed memory leaks in sphere and cylinder; no leaks are possible

--- srcs/parser/parser_cylinder.c
+++ srcs/parser/parser_cylinder.c
@@ -26,11 +26,11 @@ void	create_cylinder_node(char **tokens, t_scene *scene)
 int	check_id_cy(char **tokens)
 {
 	if (ft_array_size(tokens) != 6)
-		return (error_msg(ERROR_MANY_ARGC_CY), ERROR);
+		return (error_msg(ERROR_MANY_ARGC_CY), ft_free_array(tokens), ERROR);
 	if (check_coordinates(tokens[1]))
-		return (error_msg(ERROR_COOR_CY), ERROR);
+		return (error_msg(ERROR_COOR_CY), ft_free_array(tokens), ERROR);
 	if (check_orientation(tokens[2]))
-		return (ERROR);
+		return (ft_free_array(tokens), ERROR);
 	if (!ft_isfloat(tokens[3]) || !ft_isfloat(tokens[4]))
 		return (error_msg(ERROR_FLOAT_CY), ft_free_array(tokens), ERROR);
 	if (check_color(tokens[5]))

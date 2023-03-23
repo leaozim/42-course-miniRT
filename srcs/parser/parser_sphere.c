7a54899a1d9d4f6ee416d6e2f76b20f7f8f8eda2
Marcelo Moreira <49847614+meritissimo1@users.noreply.github.com>
2023-03-23 17:10:11 -0400

    Looking for leaks (#9)
    * fixed leaks in camera; changing some error messages
    
    * fixing bug with float number, ex: 21..42
    
    * fixed memory leaks in sphere and cylinder; no leaks are possible
    
    * checking norminette

--- srcs/parser/parser_sphere.c
+++ srcs/parser/parser_sphere.c
@@ -24,13 +24,13 @@ void	create_sphere_node(char **tokens, t_scene *scene)
 int	check_id_sp(char **tokens)
 {
 	if (ft_array_size(tokens) != 4)
-		return (error_msg(ERROR_MANY_ARGC_SP), ERROR);
+		return (error_msg(ERROR_MANY_ARGC_SP), ft_free_array(tokens), ERROR);
 	if (check_coordinates(tokens[1]))
-		return (error_msg(ERROR_ORI_SP), ERROR);
+		return (error_msg(ERROR_ORI_SP), ft_free_array(tokens), ERROR);
 	if (!ft_isfloat(tokens[2]))
-		return (error_msg(ERROR_FLOAT_SP), ERROR);
+		return (error_msg(ERROR_FLOAT_SP), ft_free_array(tokens), ERROR);
 	if (check_color(tokens[3]))
-		return (ERROR);
+		return (ft_free_array(tokens), ERROR);
 	ft_free_array(tokens);
 	return (OK);
 }
--- srcs/utils/destroy_minirt.c
+++ srcs/utils/destroy_minirt.c
@@ -13,4 +13,5 @@ void	destroy_minirt(t_scene *scene)
 	ft_lstclear(&scene->shapes, free);
     ft_lstclear(&scene->lights, free);
     free(scene->ambient);
+	free(scene->camera);
 }

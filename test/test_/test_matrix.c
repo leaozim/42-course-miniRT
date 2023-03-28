797d7c2a2723b1020d0f421b63d30f12a4b9bb90
Larissa Silva <larissa_silva@outlook.com>
2023-03-28 12:08:00 -0300

    add test create matrix

--- test/test_/test_matrix.c
+++ test/test_/test_matrix.c
@@ -2,7 +2,7 @@
 
 void	test_create_matrix_4x4()
 {
-
+	int	i, j;
 	double	m[4][4] = {
 		{1, 2, 3, 4},
 		{5, 6, 7, 8},
@@ -17,17 +17,42 @@ void	test_create_matrix_4x4()
                         {5, 4, 3, 2}
                     }};
 	TEST_ASSERT_NOT_NULL(&matrix);
-	int i = -1;
-	int j = -1;
+	i = -1;
 	while (++i < 4)
+	{
+		j = -1;
 		while (++j < 4)
 			TEST_ASSERT_EQUAL_DOUBLE(m[i][j], matrix.matrix[i][j]);
+	}
+}
 
+void	test_create_matrix_3x3()
+{
+	int		i, j;
+	double	m[3][3] = {
+		{1, 2, 3},
+		{5, 6, 7},
+		{9, 8, 7}
+	};
+	// matrix = create_matrix(m, 3); adicionar aqui a função 
+	t_matrix matrix = {3, {
+                        {1, 2, 3},
+                        {5, 6, 7},
+                        {9, 8, 85}
+                    }};
+	TEST_ASSERT_NOT_NULL(&matrix);
+	i = -1;
+	while (++i < 3)
+	{
+		j = -1;
+		while (++j < 3)
+			TEST_ASSERT_EQUAL_DOUBLE(m[i][j], matrix.matrix[i][j]);
+	}
 }
 
 void test_matrix(void)
 {
 	RUN_TEST(test_create_matrix_4x4);
-	// RUN_TEST(​test_multiplying_matrix_identity);
+	RUN_TEST(test_create_matrix_3x3);
 
 }

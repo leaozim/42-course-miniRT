fb699dce46e47f2895c376bfdd937bfb6a33c25e
Larissa Silva <larissa_silva@outlook.com>
2023-03-31 11:56:32 -0300

    add updates

--- srcs/matrix/matrix_rotations.c
+++ srcs/matrix/matrix_rotations.c
@@ -23,17 +23,3 @@ t_matrix	rotation_z(double radians)
 	rotation.matrix[1][1] = cos(radians);
 	return (rotation);
 }
-
-t_matrix	shearing_matrix(t_shearing she)
-{
-	t_matrix	shearing;
-
-	shearing = create_identity_matrix();
-	shearing.matrix[0][1] = she.x_y;
-	shearing.matrix[0][2] = she.x_z;
-	shearing.matrix[1][0] = she.y_x;
-	shearing.matrix[1][2] = she.y_z;
-	shearing.matrix[2][0] = she.z_x;
-	shearing.matrix[2][1] = she.z_y;
-	return (shearing);
-}
--- srcs/utils/destroy_minirt.c
+++ srcs/utils/destroy_minirt.c
@@ -1,13 +1,5 @@
 #include "minirt.h"
 
-void	destroy_t_shapes(void *shapes)
-{
-	t_shape	*scenelst;
-
-	scenelst = (t_shape *)shapes;
-	free(scenelst);
-}
-
 void	destroy_minirt(t_scene *scene)
 {
 	ft_lstclear(&scene->shapes, free);

cff467d594e48532d4766bea28a75a7697de9e31
meritissimo1 <marcelomoreira1905@gmail.com>
2023-04-19 09:45:56 -0400

    commenting the test test_print_three_spheres; no leaks are possible

--- test/main.c
+++ test/main.c
@@ -14,17 +14,17 @@ void tearDown(void)
 int main(void)
 {
 	UNITY_BEGIN();
-//	test_operation_tuple();
-//	test_operation_color();
-//	test_matrix();
-//	test_transformations_matrix();
-//	test_rotation();
-//	test_shearing();
-//	test_hit();
-//	test_ray();
-//	test_light_and_shading();
-//	test_world();
-//	test_view_transformation();
+	test_operation_tuple();
+	test_operation_color();
+	test_matrix();
+	test_transformations_matrix();
+	test_rotation();
+	test_shearing();
+	test_hit();
+	test_ray();
+	test_light_and_shading();
+	test_world();
+	test_view_transformation();
 	test_camera();
 	//test_canvas();
 	return (UNITY_END());
